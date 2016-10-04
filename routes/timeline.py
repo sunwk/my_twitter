82024b9f7e5d09b2d43e0a060eeed43f40ae98f4
sunwk <546602667@qq.com>
2016-10-05 01:15:12 +0800

    添加个人信息设置页面

--- routes/timeline.py
+++ routes/timeline.py
@@ -34,4 +34,4 @@ def tweet_view(user_id):
 
 @main.route('/test')
 def test_view():
-    return render_template('self_timeline_aftersign.html')
+    return render_template('personal_info_edit.html')
